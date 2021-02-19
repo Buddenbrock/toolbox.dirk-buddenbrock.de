ed7873422d8d353d536bf527363b4af5a784d32c
Dirk Buddenbrock <info@dirk-buddenbrock.de>
2021-02-19 23:49:55 +0100

    Update config

--- Settings/config.php
+++ Settings/config.php
@@ -1,22 +1,14 @@
 <?php
-
-// General
 $base = 'https://toolbox.dirk-buddenbrock.de.dev.local:8890/';
 
 $websiteTitle = 'Toolbox';
 $title = 'Sammlung von Tools';
-$description = '';
+$description = 'Die Entwickler-Toolbox bietet sortiert nach Themenfeldern eine vielzahl an nützlichen Tools, die bei der arbeit im Medien-Bereich die Arbeit erleichtern.';
 $author = 'Dirk Buddenbrock';
-$image = '';
-$imageAlt = '';
-$imageHeight = '941';
-$imageWidth = '1798';
+$image = 'Images/card-images.jpg';
+$imageAlt = 'Der Logo-Schriftzug Toolbax platziert auf blauen Grund';
+$imageHeight = '1080';
+$imageWidth = '1920';
 
-
-
-$robots = 'noindex,nofollow';
-$canonical = $base;
-$date = '2020-09-05';
 $version = '2.0.0';
-
 ?>
