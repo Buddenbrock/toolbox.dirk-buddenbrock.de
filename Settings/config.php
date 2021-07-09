aadde6cc7e7acfff7c54aa55c2ee1b82222e3ed3
Dirk Buddenbrock <info@dirk-buddenbrock.de>
2021-07-09 18:37:54 +0200

    Correct wording mistakes

--- Settings/config.php
+++ Settings/config.php
@@ -3,7 +3,7 @@ $base = 'https://toolbox.dirk-buddenbrock.de/';
 
 $websiteTitle = 'Toolbox';
 $title = 'Toolbox';
-$description = 'Die Entwickler-Toolbox bietet sortiert nach Themenfeldern eine vielzahl an nützlichen Tools, die bei der arbeit im Medien-Bereich die Arbeit erleichtern.';
+$description = 'Die Entwickler-Toolbox bietet sortiert nach Themenfeldern eine Vielzahl an nützlichen Tools, die bei der Arbeit im Medien-Bereich die Arbeit erleichtern.';
 $author = 'Dirk Buddenbrock';
 $image = 'Images/card-images.jpg';
 $imageAlt = 'Der Logo-Schriftzug Toolbax platziert auf blauen Grund';
